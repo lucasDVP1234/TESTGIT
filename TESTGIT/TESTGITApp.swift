dd5b8742ba91b7dbce6ab8ec3be64dadbf6ea6ac
Lucas Daval-Pommier <lucasdavalpommier@gmail.com>
2023-04-06 16:04:57 +0200

    Initial Commit

---
+++ TESTGIT/TESTGITApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTGITApp.swift
+//  TESTGIT
+//
+//  Created by Lucas Daval-Pommier on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TESTGITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
